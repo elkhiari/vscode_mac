21543e9356a2f35891fc72a3fe16db4fefef508d
Othmane Elkhiari <oelkhiar@e1r11p5.1337.ma>
2023-05-15 14:32:23 +0100

    add commi

--- code.sh
+++ code.sh
@@ -6,17 +6,20 @@ echo -e "
 ██║   ██║█████╗  ██║     █████╔╝ ███████║██║███████║██████╔╝
 ██║   ██║██╔══╝  ██║     ██╔═██╗ ██╔══██║██║██╔══██║██╔══██╗
 ╚██████╔╝███████╗███████╗██║  ██╗██║  ██║██║██║  ██║██║  ██║
- ╚═════╝ ╚══════╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚═╝  ╚═╝╚═╝  ╚═╝"
+ ╚═════╝ ╚══════╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚═╝  ╚═╝╚═╝  ╚═╝ \n"
 
-# Create the /usr/local/bin directory in your user's home folder if it doesn't exist
+echo "Create the /usr/local/bin directory in your user's home folder if it doesn't exist \n"
+
 mkdir -p ~/usr/local/bin
 
 sleep 1
-# Create a symbolic link to the code executable in the ~/usr/local/bin directory
+echo "Create a symbolic link to the code executable in the ~/usr/local/bin directory \n"
+
 ln -s "/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code" ~/usr/local/bin/code
 
 sleep 1
-# Add the ~/usr/local/bin directory to your PATH variable
+echo "Add the ~/usr/local/bin directory to your PATH variable \n"
+
 echo 'export PATH="$HOME/usr/local/bin:$PATH"' >> ~/..zshrc
 
 sleep 1
